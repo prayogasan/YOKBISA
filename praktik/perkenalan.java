a8bad563f7518d4396e76bf5aec60188a50ad697
prayogasan <prayogasanarisakti@gmail.com>
2021-09-26 18:56:42 +0700

    perkenalan.java

---
+++ praktik/perkenalan.java
@@ -0,0 +1,8 @@
+public class perkenalan {
+
+	public static void main(String[] args) {
+		System.out.println("Hello : Prayoga San Arisakti ");
+		System.out.println("NIM   : 6706213060");
+		System.out.println("Kelas : D3RPLA 4505");
+	}
+}
